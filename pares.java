4597d69e4866c5303ca7ccf0084823639639b2b6
Rg4ar <fanultra1223@gmail.com>
2026-02-06 14:46:37 -0400

    primer respositorio Ar

---
+++ pares.java
@@ -0,0 +1,10 @@
+public class pares {
+    public static void main(String[] args) {
+        int num = 2;
+
+        while (num <= 100) {
+            System.out.println(num);
+            num += 2;
+        }
+    }
+}
