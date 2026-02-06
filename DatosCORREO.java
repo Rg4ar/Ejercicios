3c539a21b51e53012e7abe1fc9afef1048538470
Rainiel de los Santos <124511195+httpzmiti@users.noreply.github.com>
2026-02-06 16:13:38 -0400

    Add PracticaArea1 class to display user info

---
+++ DatosCORREO.java
@@ -0,0 +1,6 @@
+public class PracticaArea1 {
+   public static void main(String[] args) {
+        System.out.println("Nombre: Luis Rainiel Perez");
+      System.out.println("Correo Electronico: Rainieldelossantos076@gmail.com");
+    }
+}
